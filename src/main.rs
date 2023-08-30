db6c2e4102b98da212cbdd5540f69cc7d7e2cb0f
lovewutong <suqin2106@gmail.com>
2023-08-30 14:40:17 +0800

    refactor:依托于epub库的epub格式电子书的阅读器

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
-use epub::doc::EpubDoc;
-use std::fs::File;
-use std::io::Read;
-use std::path::PathBuf;
+use epub;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() {
     
-    // 打开epub文件
-    let file_path = "123.epub";
-    let mut file = File::open(file_path)?;
+    let path_to_epub = "123.epub";
+    let mut book = epub::doc::EpubDoc::new(path_to_epub).expect("Failed to open EPUB file");
+    println!("EPUB file loaded successfully.");
 
-    // 读取文件内容
-    let mut epub_content = Vec::new();
-    file.read_to_end(&mut epub_content)?;
+    // 打印元数据
+    let title = book
+        .metadata
+        .get("title")
+        .and_then(|v| v.get(0))
+        .cloned()
+        .unwrap_or_else(|| String::from("Unknown"));
+    let authors = book
+        .metadata
+        .get("creator")
+        .and_then(|v| v.get(0))
+        .cloned()
+        .unwrap_or_else(|| String::from("Unknown"));
 
-    // 创建EpubDoc对象
-    let epub_doc = EpubDoc::new(&epub_content)?;
+    println!("Title: {:?}", title);
+    println!("Metadata: {:?}", book.metadata);
+    println!("Authors: {:?}", authors);
 
-    // 获取元数据信息
-    let metadata = epub_doc.metadata();
+    // 打印每章节的内容
+    let items: Vec<_> = book.spine.clone();
+    println!("Number of spine items: {}", items.len());
 
-    // 打印元数据信息
-    println!("Tittle is :{}",metadata.title);
-    println!("Author is :{}",metadata.author);
-    println!("Description is :{}",metadata.description);
+    for item_id in items.iter() {
+        let path_option = book.resources.get(item_id).map(|(p, _)| p.clone());
+        if let Some(path) = path_option {
+            if let Some(resource) = book.get_resource_by_path(&path) {
+                let content_as_str =
+                    std::str::from_utf8(&resource).expect("Failed to convert to string");
+                println!("---\n{}", content_as_str);
+            } else {
+                println!("Warning: Failed to get resource for item: {:?}", item_id);
+            }
+        }
+    }
 
-    Ok(())
+    // Print the resources in the EPUB file
+    println!("\nResources in the EPUB file:");
+    for (resource_id, (path, mime)) in &book.resources {
+        println!("ID: {}, Path: {:?}, MIME: {}", resource_id, path, mime);
+    }
 }
